285d28bdfe14fa94ab1c7c5ee625a5bc106adb6a
keef hassan <keefhassan4@gmail.comgit config --global user.email keefhassan4@gmail.coome>
2024-03-01 21:11:45 -0600

    create details route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,21 @@
 import { Route, HashRouter as Router } from 'react-router-dom';
 import MovieList from '../MovieList/MovieList';
 import './App.css';
+import MovieDetails from '../movieDetails/movieDetails';
 
 function App() {
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
       <Router>        
         <Route path="/" exact>
+        <h1>The Movies Saga!</h1>
+
           <MovieList />
         </Route>
-        
+
+        <Route path="/details/:id" exact>
+          <MovieDetails />
+        </Route>
         {/* Details page */}
 
         {/* Add Movie page */}
