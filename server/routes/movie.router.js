4be57b4ca0f52d3ee1c59768920f409366b7fce5
keef hassan <keefhassan4@gmail.comgit config --global user.email keefhassan4@gmail.coome>
2024-03-01 21:10:53 -0600

    create movie details

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -17,6 +17,35 @@ router.get('/', (req, res) => {
     })
 
 });
+router.get('/:id', (req, res) => {
+  const query = `
+  SELECT 
+  m.id AS movie_id,
+  m.title AS movie_title,
+  m.poster AS movie_poster,
+  m.description AS movie_description,
+  STRING_AGG(g.name, ', ') AS genres
+FROM 
+  movies AS m
+JOIN 
+  movies_genres AS mg ON m.id = mg.movie_id
+JOIN 
+  genres AS g ON mg.genre_id = g.id
+WHERE
+  m.id = $1
+GROUP BY
+  m.id, m.title, m.poster, m.description;
+  `;
+  pool.query(query,[req.params.id])
+    .then(result => {
+      res.send(result.rows[0]);
+    })
+    .catch(err => {
+      console.log('ERROR: Get all movies', err);
+      res.sendStatus(500)
+    })
+
+});
 
 router.post('/', (req, res) => {
   console.log(req.body);
